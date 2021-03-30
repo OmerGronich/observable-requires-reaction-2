26bbd3a11e741e19a2d7268500d4ef63fb3c2453
omergronich <gronichomer@gmail.com>
2021-03-30 15:20:12 +0300

    Recreated the bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,42 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { configure, makeAutoObservable }      from 'mobx';
+
+configure({observableRequiresReaction: true});
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
-  `,
-  styles: []
-})
+			   selector       : 'app-root',
+			   template       : `
+                   <h2>Open the console to see the warning</h2>
+                   <div *mobxAutorun>
+                       counter 1 (autorun): {{counter1}}
+                       <button (click)="increment1()">+</button>
+                   </div>
+                   <div *mobxReaction="getCounter2.bind(this)">
+                       counter 2 (autorun): {{counter2}}
+                       <button (click)="increment2()">+</button>
+                   </div>
+			   `,
+			   changeDetection: ChangeDetectionStrategy.OnPush,
+			   styles         : []
+		   })
 export class AppComponent {
-  title = 'observable-requires-reaction2';
+
+	counter1 = 0;
+	counter2 = 0;
+
+	constructor() {
+		makeAutoObservable(this);
+	}
+
+	getCounter2() {
+		return this.counter2;
+	}
+
+	increment1() {
+		this.counter1++;
+	}
+
+	increment2() {
+		this.counter2++;
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent }      from './app.component';
+import { MobxAngularModule } from "mobx-angular";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+			  declarations: [
+				  AppComponent
+			  ],
+			  imports     : [
+				  BrowserModule,
+				  MobxAngularModule
+			  ],
+			  providers   : [],
+			  bootstrap   : [AppComponent]
+		  })
+export class AppModule {
+}
